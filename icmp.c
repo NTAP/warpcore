ad7cd2f08899f6adf4f93f447df032c9e1023c25
Lars Eggert <lars@netapp.com>
2014-09-04 10:41:16 +0200

    Support for generating and sending various ICMP "unreachable" messages.

--- icmp.c
+++ icmp.c
@@ -3,8 +3,31 @@
 #include "ip.h"
 
 
-void icmp_tx(const struct warpcore * const w, const char * const buf, const uint16_t offset, const uint16_t len) {
-	struct icmp_hdr * const icmp = (struct icmp_hdr * const)(buf + offset);
+void icmp_tx_unreach(const struct warpcore * const w, const uint_fast8_t code,
+	char * const buf, const uint_fast16_t off) {
+	// make an ICMP unreachable out of this received packet
+	// copy IP hdr + 64 bytes of the original IP packet as the ICMP payload
+	struct ip_hdr * const ip =
+		(struct ip_hdr * const)(buf + sizeof(struct eth_hdr));
+	const uint_fast16_t len = ip->hl * 4 + 64;
+	memmove(buf + off + sizeof(struct icmp_hdr) + 4, ip, len);
+
+	// insert an ICMP header
+	struct icmp_hdr * const icmp = (struct icmp_hdr * const)(buf + off);
+	icmp->type = ICMP_TYPE_UNREACH;
+	icmp->code = code;
+
+	// TODO: implement RFC4884 instead of setting the padding to zero
+	uint32_t *pad = (uint32_t *)(buf + off + sizeof(struct icmp_hdr));
+	*pad = 0;
+
+	icmp_tx(w, buf, off, sizeof(struct icmp_hdr) + 4 + len); // calculates cksum
+}
+
+
+void icmp_tx(const struct warpcore * const w, const char * const buf,
+	const uint_fast16_t off, const uint_fast16_t len) {
+	struct icmp_hdr * const icmp = (struct icmp_hdr * const)(buf + off);
 
 	D("ICMP type %d, code %d", icmp->type, icmp->code);
 
@@ -13,12 +36,13 @@ void icmp_tx(const struct warpcore * const w, const char * const buf, const uint
 	icmp->cksum = in_cksum(icmp, len);
 
 	// do IP transmit preparation
-	ip_tx(w, buf);
+	ip_tx(w, IP_P_ICMP, buf, len);
 }
 
 
-void icmp_rx(const struct warpcore * const w, const char * const buf, const uint16_t offset, const uint16_t len) {
-	struct icmp_hdr * const icmp = (struct icmp_hdr * const)(buf + offset);
+void icmp_rx(const struct warpcore * const w, char * const buf,
+	const uint_fast16_t off, const uint_fast16_t len) {
+	struct icmp_hdr * const icmp = (struct icmp_hdr * const)(buf + off);
 
 	D("ICMP type %d, code %d", icmp->type, icmp->code);
 
@@ -28,7 +52,7 @@ void icmp_rx(const struct warpcore * const w, const char * const buf, const uint
 		case ICMP_TYPE_ECHO:
 			// transform the received echo into an echo reply and send it
 			icmp->type = ICMP_TYPE_ECHOREPLY;
-			icmp_tx(w, buf, offset, len);
+			icmp_tx(w, buf, off, len);
 			break;
 		default:
 			D("unhandled ICMP type %d", icmp->type);
--- icmp.h
+++ icmp.h
@@ -3,16 +3,28 @@
 
 #include <stdint.h>
 
-static const uint_fast8_t ICMP_TYPE_ECHOREPLY =	0;
-static const uint_fast8_t ICMP_TYPE_ECHO = 		8;
+static const uint_fast8_t ICMP_TYPE_ECHOREPLY =		0;
+static const uint_fast8_t ICMP_TYPE_UNREACH =		3;
+static const uint_fast8_t ICMP_TYPE_ECHO = 			8;
+
+static const uint_fast8_t ICMP_UNREACH_PROTOCOL =	2;	// bad protocol
+static const uint_fast8_t ICMP_UNREACH_PORT =		3;	// bad port
 
 struct icmp_hdr {
 	uint8_t		type;	// type of message
 	uint8_t		code;	// type sub code
 	uint16_t	cksum;	// ones complement checksum of struct
-};
+} __packed;
 
 struct warpcore;
-extern void icmp_rx(const struct warpcore * const w, const char * const buf, const uint16_t offset, const uint16_t len);
+
+extern void icmp_tx_unreach(const struct warpcore * const w,
+	const uint_fast8_t code, char * const buf, const uint_fast16_t off);
+
+extern void icmp_tx(const struct warpcore * const w, const char * const buf,
+	const uint_fast16_t off, const uint_fast16_t len);
+
+extern void icmp_rx(const struct warpcore * const w, char * const buf,
+	const uint_fast16_t off, const uint_fast16_t len);
 
 #endif
