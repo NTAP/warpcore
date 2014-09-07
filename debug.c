d2b3ff1e4b765c903e690e80947721ec855cdb4e
Lars Eggert <lars@netapp.com>
2014-09-07 13:45:51 +0200

    Fix hexdump formatting

--- debug.c
+++ debug.c
@@ -1,17 +1,16 @@
-#include <stdio.h>
-#include <ctype.h>
+#include <stdio.h>  // printf
+#include <ctype.h>  // isprint
 
 #include "debug.h"
 
 void hexdump(const void * const ptr, const unsigned len)
 {
 	const char * const buf = (const char * const)ptr;
-
 	for (unsigned i = 0; i < len; i += 16) {
 		fprintf(stderr, "%06x: ", i);
 		for (unsigned j = 0; j < 16; j++)
 			if (i+j < len)
-				fprintf(stderr, "%02x ", buf[i+j]);
+				fprintf(stderr, "%02hhx ", buf[i+j]);
 			else
 				fprintf(stderr, "   ");
 			fprintf(stderr, " ");
