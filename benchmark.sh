95220f0976aa5687949eb20bafc11ac65485e071
Lars Eggert <lars@netapp.com>
2014-09-18 19:11:03 +0200

    More robust remote operations

--- benchmark.sh
+++ benchmark.sh
@@ -6,7 +6,7 @@ iface=ix0
 peer=mora2
 peerip=172.16.5.32
 
-cmd="FreeBSD/warpping -i $iface -d $peerip -l 20000"
+cmd="FreeBSD/warpping -i $iface -d $peerip -l 10000"
 
 if [ -z "$(ifconfig $iface | grep 'inet ')" ]; then
 	echo local interface has no IP
@@ -19,8 +19,8 @@ if [ -z "$(ssh $peer ifconfig $iface | grep 'inet ')" ]; then
 fi
 
 # # kill dhclient during the time the interfaces are in netmap mode
-sudo pkill -f "dhclient.*$iface" || true
-ssh $peer "sudo pkill -f 'dhclient.*$iface'" || true
+sudo pkill -INT -f "dhclient.*$iface" || true
+ssh $peer "sudo pkill -INT -f 'dhclient.*$iface'" || true
 
 run () {
 	local flag
@@ -28,22 +28,23 @@ run () {
 		flag=-k
 	fi
 #	for size in 16 100 200 300 400 500 600 700 800 900 1000 1100 1200 1300 1400 1458; do
-	for (( size=16; size <= 1458; size+=103)); do
+#	for (( size=16; size <= 1458; size+=103)); do
+	for (( size=16; size <= 1458; size+=303)); do
 	# for (( size=16; size <= 1458; size+=503)); do
-		echo Running $i size $size
-		$cmd $flag -s $size > $1.$size.txt
+		echo "Running $1 size $size"
+		$cmd $flag -s $size > "$1.$size.txt"
 	done
 }
 
 
-ssh $peer sudo pkill warpinetd || true
+ssh $peer sudo pkill -INT warpinetd || true
 ssh $peer 'cd ~/warpcore; nohup FreeBSD/warpinetd -i ix0 > warpinetd.log 2>&1 &'
 run warp
-ssh $peer sudo pkill warpinetd || true
+ssh $peer sudo pkill -INT warpinetd || true
 
 sleep 3
 run kern
 
 # restart dhclient
 sudo dhclient $iface
-ssh $peer sudo dhclient $iface
+ssh $peer "sudo dhclient $iface"
