3f3ef6266fd44143104f9dc382ad391164da4e06
Lars Eggert <lars@netapp.com>
2014-09-18 15:53:37 +0200

    Various fixes

--- benchmark.sh
+++ benchmark.sh
@@ -1,33 +1,49 @@
-#! /bin/sh
+#! /usr/bin/env bash
+
+set -e
 
 iface=ix0
-cmd="FreeBSD/warpping -i $iface -d 172.16.20.1 -l 10000"
+peer=mora2
+peerip=172.16.5.32
 
+cmd="FreeBSD/warpping -i $iface -d $peerip -l 20000"
 
 if [ -z "$(ifconfig $iface | grep 'inet ')" ]; then
 	echo local interface has no IP
 	exit
 fi
 
-if [ -z "$(ssh mora2 ifconfig $iface | grep 'inet ')" ]; then
+if [ -z "$(ssh $peer ifconfig $iface | grep 'inet ')" ]; then
         echo remote interface has no IP
         exit
 fi
 
+# # kill dhclient during the time the interfaces are in netmap mode
+sudo pkill -f "dhclient.*$iface" || true
+ssh $peer "sudo pkill -f 'dhclient.*$iface'" || true
 
 run () {
 	local flag
 	if [ "$1" == "kern" ]; then
 		flag=-k
 	fi
-	for size in 16 32 64 128 256 512 1024 1450; do
+#	for size in 16 100 200 300 400 500 600 700 800 900 1000 1100 1200 1300 1400 1458; do
+	for (( size=16; size <= 1458; size+=103)); do
+	# for (( size=16; size <= 1458; size+=503)); do
+		echo Running $i size $size
 		$cmd $flag -s $size > $1.$size.txt
 	done
 }
 
 
+ssh $peer sudo pkill warpinetd || true
+ssh $peer 'cd ~/warpcore; nohup FreeBSD/warpinetd -i ix0 > warpinetd.log 2>&1 &'
+run warp
+ssh $peer sudo pkill warpinetd || true
+
+sleep 3
 run kern
 
-pid=$(ssh mora2 'cd ~/warpcore; nohup FreeBSD/warpinetd -i ix0 > warpinetd.log 2>&1 & echo $!')
-run warp
-ssh mora2 kill $pid
+# restart dhclient
+sudo dhclient $iface
+ssh $peer sudo dhclient $iface
