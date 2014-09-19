316582c425c2a765055ffa711e86db6abeaacdac
Lars Eggert <lars@netapp.com>
2014-09-19 08:44:16 +0200

    Peer interface may have different name than local

--- benchmark.sh
+++ benchmark.sh
@@ -2,35 +2,34 @@
 
 set -e
 
-iface=ix0
-peer=mora2
-peerip=172.16.5.32
+iface=em1
+peeriface=eth1
+peer=tux
+peerip=192.168.125.129
 
 cmd="FreeBSD/warpping -i $iface -d $peerip -l 10000"
 
-if [ -z "$(ifconfig $iface | grep 'inet ')" ]; then
+if [ -z "$(/sbin/ifconfig $iface | grep 'inet ')" ]; then
 	echo local interface has no IP
 	exit
 fi
 
-if [ -z "$(ssh $peer ifconfig $iface | grep 'inet ')" ]; then
+if [ -z "$(ssh $peer /sbin/ifconfig $peeriface | grep 'inet ')" ]; then
         echo remote interface has no IP
         exit
 fi
 
 # # kill dhclient during the time the interfaces are in netmap mode
 sudo pkill -INT -f "dhclient.*$iface" || true
-ssh $peer "sudo pkill -INT -f 'dhclient.*$iface'" || true
+ssh $peer "sudo pkill -INT -f 'dhclient.*$peeriface'" || true
 
 run () {
 	local flag
 	if [ "$1" == "kern" ]; then
 		flag=-k
 	fi
-#	for size in 16 100 200 300 400 500 600 700 800 900 1000 1100 1200 1300 1400 1458; do
-#	for (( size=16; size <= 1458; size+=103)); do
-	for (( size=16; size <= 1458; size+=303)); do
-	# for (( size=16; size <= 1458; size+=503)); do
+	for (( size=16; size <= 1458; size+=103)); do
+	# for (( size=16; size <= 1458; size+=303)); do
 		echo "Running $1 size $size"
 		$cmd $flag -s $size > "$1.$size.txt"
 	done
@@ -42,9 +41,9 @@ ssh $peer 'cd ~/warpcore; nohup FreeBSD/warpinetd -i ix0 > warpinetd.log 2>&1 &'
 run warp
 ssh $peer sudo pkill -INT warpinetd || true
 
-sleep 3
-run kern
+# sleep 3
+# run kern
 
 # restart dhclient
 sudo dhclient $iface
-ssh $peer "sudo dhclient $iface"
+ssh $peer "sudo dhclient $peeriface"
