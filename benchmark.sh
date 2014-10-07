0521664a1d2d304823667fc4d2264b01f2a84f39
Lars Eggert <lars@netapp.com>
2014-10-07 16:07:38 +0200

    Changes so FreeBSD<->Linux testing works

--- benchmark.sh
+++ benchmark.sh
@@ -2,27 +2,31 @@
 
 set -e
 
-iface=ix0
-piface=ix0
+loops=1000
+# busywait=-b
+peer=tux
+iface=em1
+piface=eth1
+
 # iface=igb0
 # piface=igb0
-iname=$(echo $iface | tr -d 0-9)
-peer=six
-peerip=$(ssh $peer "/sbin/ifconfig $piface | grep 'inet ' | cut -f 2 -d' '")
-loops=100000
 
-busywait=-b
+iname=$(echo $iface | tr -d 0-9)
+peerip=$(ssh $peer "/sbin/ifconfig $piface" | sed -e s/addr:// -e s/^[[:space:]]*//g | grep 'inet ' | cut -f 2 -d' ')
+
+os=$(uname -s)
+peeros=$(ssh $peer "uname -s")
 
 run () {
 	local flag
 	if [ "$1" == "kern" ]; then
 		flag=-k
 	fi
-	local cmd="FreeBSD/warpping -i $iface -d $peerip -l $loops $busywait"
-	for (( size=16; size <= 1458; size+=103)); do
-	# for (( size=16; size <= 1458; size+=303)); do
+	local cmd="$os/warpping -i $iface -d $peerip -l $loops $busywait"
+	# for (( size=16; size <= 1458; size+=103)); do
+	for (( size=16; size <= 1458; size+=303)); do
 		echo "Running $1 size $size"
-		$cmd $flag -s $size >> "$1.txt"
+		$cmd $flag -s $size >> "$1.txt" 2> warpping.$1.log
 	done
 	echo "us	codeus	size" > $1.new
 	grep -v codeus $1.txt >> $1.new
@@ -48,13 +52,13 @@ sudo sysctl hw.$iname.enable_aim=0 || true
 sudo cpuset -l 1 -p $(pgrep ^inetd)
 
 ssh $peer "sudo pkill -f 'dhclient.*$piface'" || true
-ssh $peer "pkill -INT -f warpinetd; pkill -INT -f warpinetd" || true
+ssh $peer "pkill -INT -f warpinetd; sleep 1; pkill -INT -f warpinetd" || true
 ssh $peer "sudo sysctl hw.$iname.enable_aim=0" || true
 ssh $peer 'sudo cpuset -l 1 -p $(pgrep ^inetd)' || true
 
-ssh $peer "cd ~/warpcore; nohup FreeBSD/warpinetd -i $piface $busywait > warpinetd.log 2>&1 &" || true
+ssh $peer "cd ~/warpcore; nohup $peeros/warpinetd -i $piface $busywait > warpinetd.log 2>&1 &" || true
 run warp
-ssh $peer "pkill -INT -f warpinetd; pkill -INT -f warpinetd" || true
+ssh $peer "pkill -INT -f warpinetd; sleep 1; pkill -INT -f warpinetd" || true
 
 ssh $peer "sudo dhclient $piface" || true
 sleep 3
