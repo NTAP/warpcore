NTAP/warpcore

commit 4a5df912b2de69f46d1cbfbebe1c996e493169f0

Use assert()

lib/warpcore.c
@@ -45,8 +45,7 @@ struct w_iov * w_tx_alloc(struct w_sock * const s, const uint32_t len)
     while (l > 0) {
         // grab a spare buffer
         v = STAILQ_FIRST(&s->w->iov);
-        if (unlikely(v == 0))
-            die("out of spare bufs after grabbing %d", n);
+        assert(v != 0, "out of spare bufs after grabbing %d", n);
         STAILQ_REMOVE_HEAD(&s->w->iov, next);
         warn(debug, "grabbing spare buf %d for user tx", v->idx);
         v->buf = IDX2BUF(s->w, v->idx) + s->hdr_len;
@@ -134,16 +133,14 @@ struct w_iov * w_rx(struct w_sock * const s)
 // Internal warpcore function. Kick the tx ring.
 void w_kick_tx(const struct warpcore * const w)
 {
-    if (unlikely(ioctl(w->fd, NIOCTXSYNC, 0) == -1))
-        die("cannot kick tx ring");
+    assert(ioctl(w->fd, NIOCTXSYNC, 0) != -1, "cannot kick tx ring");
 }
 
 
 // Internal warpcore function. Kick the rx ring.
 void w_kick_rx(const struct warpcore * const w)
 {
-    if (unlikely(ioctl(w->fd, NIOCRXSYNC, 0) == -1))
-        die("cannot kick rx ring");
+    assert(ioctl(w->fd, NIOCRXSYNC, 0) != -1, "cannot kick rx ring");
 }
 
 
@@ -245,8 +242,8 @@ w_bind(struct warpcore * const w, const uint8_t p, const uint16_t port)
         return 0;
     }
 
-    if ((*s = calloc(1, sizeof(struct w_sock))) == 0)
-        die("cannot allocate struct w_sock");
+    assert((*s = calloc(1, sizeof(struct w_sock))) != 0,
+           "cannot allocate struct w_sock");
 
     (*s)->p = p;
     (*s)->sport = port;
@@ -257,8 +254,7 @@ w_bind(struct warpcore * const w, const uint8_t p, const uint16_t port)
     // initialize the non-zero fields of outgoing template header
     const uint16_t hl =
         sizeof(struct eth_hdr) + sizeof(struct ip_hdr) + sizeof(struct udp_hdr);
-    if (((*s)->hdr = calloc(1, hl)) == 0)
-        die("cannot allocate w->hdr");
+    assert(((*s)->hdr = calloc(1, hl)) != 0, "cannot allocate w->hdr");
 
     struct eth_hdr * const eth = (struct eth_hdr *)(*s)->hdr;
     eth->type = ETH_TYPE_IP;
@@ -347,11 +343,10 @@ void w_cleanup(struct warpcore * const w)
     }
     w->nif->ni_bufs_head = STAILQ_FIRST(&w->iov)->idx;
 
-    if (munmap(w->mem, w->req.nr_memsize) == -1)
-        die("cannot munmap netmap memory");
+    assert(munmap(w->mem, w->req.nr_memsize) != -1,
+           "cannot munmap netmap memory");
 
-    if (close(w->fd) == -1)
-        die("cannot close /dev/netmap");
+    assert(close(w->fd) != -1, "cannot close /dev/netmap");
 
     // free extra buffer list
     while (!STAILQ_EMPTY(&w->iov)) {
@@ -384,8 +379,7 @@ void w_init_common(void)
     plat_setaffinity();
 
     // lock memory
-    if (mlockall(MCL_CURRENT | MCL_FUTURE) == -1)
-        die("mlockall");
+    assert(mlockall(MCL_CURRENT | MCL_FUTURE) != -1, "mlockall");
 }
 
 
@@ -395,12 +389,11 @@ struct warpcore * w_init(const char * const ifname)
     struct warpcore * w;
     bool link_up = false;
 
-    if (_w)
-        die("can only have one warpcore engine active");
+    assert(_w == 0, "can only have one warpcore engine active");
 
     // allocate struct
-    if ((w = calloc(1, sizeof(struct warpcore))) == 0)
-        die("cannot allocate struct warpcore");
+    assert((w = calloc(1, sizeof(struct warpcore))) != 0,
+           "cannot allocate struct warpcore");
 
     // we mostly loop here because the link may be down
     while (link_up == false || IS_ZERO(w->mac) || w->mtu == 0 || w->mbps == 0 ||
@@ -408,8 +401,8 @@ struct warpcore * w_init(const char * const ifname)
 
         // get interface information
         struct ifaddrs * ifap;
-        if (getifaddrs(&ifap) == -1)
-            die("%s: cannot get interface information", ifname);
+        assert(getifaddrs(&ifap) != -1, "%s: cannot get interface information",
+               ifname);
 
         for (const struct ifaddrs * i = ifap; i->ifa_next; i = i->ifa_next) {
             if (strcmp(i->ifa_name, ifname) != 0)
@@ -447,8 +440,8 @@ struct warpcore * w_init(const char * const ifname)
         freeifaddrs(ifap);
         sleep(1);
     }
-    if (w->ip == 0 || w->mask == 0 || w->mtu == 0 || IS_ZERO(w->mac))
-        die("%s: cannot obtain needed interface information", ifname);
+    assert(w->ip != 0 && w->mask != 0 && w->mtu != 0 && !IS_ZERO(w->mac),
+           "%s: cannot obtain needed interface information", ifname);
 
     w->bcast = w->ip | (~w->mask);
 
@@ -464,8 +457,8 @@ struct warpcore * w_init(const char * const ifname)
 #endif
 
     // open /dev/netmap
-    if ((w->fd = open("/dev/netmap", O_RDWR)) == -1)
-        die("cannot open /dev/netmap");
+    assert((w->fd = open("/dev/netmap", O_RDWR)) != -1,
+           "cannot open /dev/netmap");
 
     // switch interface to netmap mode
     strncpy(w->req.nr_name, ifname, sizeof w->req.nr_name);
@@ -476,15 +469,15 @@ struct warpcore * w_init(const char * const ifname)
     w->req.nr_ringid |= NETMAP_NO_TX_POLL;
     w->req.nr_flags = NR_REG_ALL_NIC;
     w->req.nr_arg3 = NUM_EXTRA_BUFS; // request extra buffers
-    if (ioctl(w->fd, NIOCREGIF, &w->req) == -1)
-        die("%s: cannot put interface into netmap mode", ifname);
+    assert(ioctl(w->fd, NIOCREGIF, &w->req) != -1,
+           "%s: cannot put interface into netmap mode", ifname);
 
     // mmap the buffer region
     // TODO: see TODO in nm_open() in netmap_user.h
     const int flags = PLAT_MMFLAGS;
-    if ((w->mem = mmap(0, w->req.nr_memsize, PROT_WRITE | PROT_READ,
-                       MAP_SHARED | flags, w->fd, 0)) == MAP_FAILED)
-        die("cannot mmap netmap memory");
+    assert((w->mem = mmap(0, w->req.nr_memsize, PROT_WRITE | PROT_READ,
+                          MAP_SHARED | flags, w->fd, 0)) != MAP_FAILED,
+           "cannot mmap netmap memory");
 
     // direct pointer to the netmap interface struct for convenience
     w->nif = NETMAP_IF(w->mem, w->req.nr_offset);
@@ -507,8 +500,7 @@ struct warpcore * w_init(const char * const ifname)
     STAILQ_INIT(&w->iov);
     for (uint32_t n = 0, i = w->nif->ni_bufs_head; n < w->req.nr_arg3; n++) {
         struct w_iov * const v = calloc(1, sizeof(struct w_iov));
-        if (v == 0)
-            die("cannot allocate w_iov");
+        assert(v != 0, "cannot allocate w_iov");
         v->buf = IDX2BUF(w, i);
         v->idx = i;
         STAILQ_INSERT_HEAD(&w->iov, v, next);
@@ -527,17 +519,17 @@ struct warpcore * w_init(const char * const ifname)
     SLIST_INIT(&w->sock);
 
     // allocate socket pointers
-    if ((w->udp = calloc(UINT16_MAX, sizeof(struct w_sock *))) == 0)
-        die("cannot allocate UDP sockets");
+    assert((w->udp = calloc(UINT16_MAX, sizeof(struct w_sock *))) == 0,
+           "cannot allocate UDP sockets");
 
     // do the common system setup which is also useful for non-warpcore
     w_init_common();
 
     // block SIGINT and SIGTERM
-    if (signal(SIGINT, w_handler) == SIG_ERR)
-        die("cannot register SIGINT handler");
-    if (signal(SIGTERM, w_handler) == SIG_ERR)
-        die("cannot register SIGTERM handler");
+    assert(signal(SIGINT, w_handler) != SIG_ERR,
+           "cannot register SIGINT handler");
+    assert(signal(SIGTERM, w_handler) != SIG_ERR,
+           "cannot register SIGTERM handler");
 
     _w = w;
     return w;

lib/arp.c
@@ -19,7 +19,7 @@
 
 // This modifies the ARP query in the current receive buffer into an ARP reply
 // and sends it out.
-static void arp_is_at(struct warpcore * w, char * const buf)
+static void arp_is_at(struct warpcore * w, void * const buf)
 {
     struct arp_hdr * const arp = eth_data(buf);
 
@@ -49,8 +49,7 @@ void arp_who_has(struct warpcore * const w, const uint32_t dip)
 {
     // grab a spare buffer
     struct w_iov * const v = STAILQ_FIRST(&w->iov);
-    if (v == 0)
-        die("out of spare bufs");
+    assert(v != 0, "out of spare bufs");
     STAILQ_REMOVE_HEAD(&w->iov, next);
     v->buf = IDX2BUF(w, v->idx);
 
@@ -92,7 +91,7 @@ void arp_who_has(struct warpcore * const w, const uint32_t dip)
 
 
 // Receive an ARP packet, and react
-void arp_rx(struct warpcore * w, char * const buf)
+void arp_rx(struct warpcore * w, void * const buf)
 {
 #ifndef NDEBUG
     char tpa[IP_ADDR_STRLEN];
@@ -102,12 +101,12 @@ void arp_rx(struct warpcore * w, char * const buf)
     const struct arp_hdr * const arp = eth_data(buf);
     const uint16_t hrd = ntohs(arp->hrd);
 
-    if (hrd != ARP_HRD_ETHER || arp->hln != ETH_ADDR_LEN)
-        die("unhandled ARP hardware format %d with len %d", hrd, arp->hln);
+    assert(hrd == ARP_HRD_ETHER && arp->hln == ETH_ADDR_LEN,
+           "unhandled ARP hardware format %d with len %d", hrd, arp->hln);
 
-    if (arp->pro != ETH_TYPE_IP || arp->pln != IP_ADDR_LEN)
-        die("unhandled ARP protocol format %d with len %d", ntohs(arp->pro),
-            arp->pln);
+    assert(arp->pro == ETH_TYPE_IP && arp->pln == IP_ADDR_LEN,
+           "unhandled ARP protocol format %d with len %d", ntohs(arp->pro),
+           arp->pln);
 
     const uint16_t op = ntohs(arp->op);
     switch (op) {

lib/arp.h
@@ -30,6 +30,6 @@ struct warpcore;
 
 // see arp.c for documentation of functions
 
-extern void arp_rx(struct warpcore * w, char * const buf);
+extern void arp_rx(struct warpcore * w, void * const buf);
 
 extern void arp_who_has(struct warpcore * const w, const uint32_t dip);

lib/ip.c
@@ -60,8 +60,8 @@ void ip_tx_with_rx_buf(struct warpcore * w,
 
     // TODO: we should zero out any IP options here,
     // since we're reflecing a received packet
-    if (ip_hl(ip) > sizeof(struct ip_hdr))
-        die("original packet seems to have IP options");
+    assert(ip_hl(ip) <= sizeof(struct ip_hdr),
+           "original packet seems to have IP options");
 
     // make the original IP src address the new dst, and set the src
     ip->dst = ip->src;
@@ -111,12 +111,10 @@ void ip_rx(struct warpcore * const w, void * const buf)
     }
 
     // TODO: handle IP options
-    if (unlikely(ip_hl(ip) != 20))
-        die("no support for IP options");
+    assert(ip_hl(ip) == 20, "no support for IP options");
 
     // TODO: handle IP fragments
-    if (unlikely(ntohs(ip->off) & IP_OFFMASK))
-        die("no support for IP options");
+    assert((ntohs(ip->off) & IP_OFFMASK) == 0, "no support for IP options");
 
     if (likely(ip->p == IP_P_UDP))
         udp_rx(w, buf, ip->src);

lib/plat_freebsd.c
@@ -35,8 +35,7 @@ void plat_setaffinity(void)
     for (i = CPU_SETSIZE - 1; i >= 0; i--)
         if (CPU_ISSET(i, &myset))
             break;
-    if (i == 0)
-        die("not allowed to run on any CPUs!?");
+    assert(i != 0, "not allowed to run on any CPUs!?");
 
     // Set new CPU mask
     warn(info, "setting affinity to CPU %d", i);

lib/plat_linux.c
@@ -23,23 +23,22 @@ void plat_setaffinity(void)
 {
     int i;
     cpu_set_t myset;
-    if (sched_getaffinity(0, sizeof(cpu_set_t), &myset) == -1)
-        die("sched_getaffinity");
+    assert(sched_getaffinity(0, sizeof(cpu_set_t), &myset) != -1,
+           "sched_getaffinity");
 
     // Find last available CPU
     for (i = CPU_SETSIZE - 1; i >= -1; i--)
         if (CPU_ISSET(i, &myset))
             break;
-    if (i == -1)
-        die("not allowed to run on any CPUs!?");
+    assert(i != -1, "not allowed to run on any CPUs!?");
 
     // Set new CPU mask
     warn(info, "setting affinity to CPU %d", i);
     CPU_ZERO(&myset);
     CPU_SET(i, &myset);
 
-    if (sched_setaffinity(0, sizeof(myset), &myset) == -1)
-        die("sched_setaffinity");
+    assert(sched_setaffinity(0, sizeof(myset), &myset) != -1,
+           "sched_setaffinity");
 }
 
 
@@ -52,16 +51,14 @@ void plat_get_mac(uint8_t * mac, const struct ifaddrs * i)
 
 uint16_t plat_get_mtu(const struct ifaddrs * i)
 {
-    int s;
-    if ((s = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
-        die("%s socket", i->ifa_name);
+    const int s = socket(AF_INET, SOCK_DGRAM, 0);
+    assert(s >= 0, "%s socket", i->ifa_name);
 
     struct ifreq ifr;
     bzero(&ifr, sizeof(struct ifreq));
     strcpy(ifr.ifr_name, i->ifa_name);
 
-    if (ioctl(s, SIOCGIFMTU, &ifr) < 0)
-        die("%s ioctl", i->ifa_name);
+    assert(ioctl(s, SIOCGIFMTU, &ifr) >= 0, "%s ioctl", i->ifa_name);
 
     const uint16_t mtu = (uint16_t)ifr.ifr_ifru.ifru_mtu;
     close(s);
@@ -72,9 +69,8 @@ uint16_t plat_get_mtu(const struct ifaddrs * i)
 
 uint32_t plat_get_mbps(const struct ifaddrs * i)
 {
-    int s;
-    if ((s = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
-        die("%s socket", i->ifa_name);
+    const int s = socket(AF_INET, SOCK_DGRAM, 0);
+    assert(s >= 0, "%s socket", i->ifa_name);
 
     struct ifreq ifr;
     bzero(&ifr, sizeof(struct ifreq));
@@ -83,8 +79,7 @@ uint32_t plat_get_mbps(const struct ifaddrs * i)
     struct ethtool_cmd edata;
     ifr.ifr_data = (__caddr_t)&edata;
     edata.cmd = ETHTOOL_GSET;
-    if (ioctl(s, SIOCETHTOOL, &ifr) < 0)
-        die("%s ioctl", i->ifa_name);
+    assert(ioctl(s, SIOCETHTOOL, &ifr) >= 0, "%s ioctl", i->ifa_name);
 
     return ethtool_cmd_speed(&edata);
 }
@@ -92,16 +87,14 @@ uint32_t plat_get_mbps(const struct ifaddrs * i)
 
 bool plat_get_link(const struct ifaddrs * i)
 {
-    int s;
-    if ((s = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
-        die("%s socket", i->ifa_name);
+    const int s = socket(AF_INET, SOCK_DGRAM, 0);
+    assert(s >= 0, "%s socket", i->ifa_name);
 
     struct ifreq ifr;
     bzero(&ifr, sizeof(struct ifreq));
     strcpy(ifr.ifr_name, i->ifa_name);
 
-    if (ioctl(s, SIOCGIFFLAGS, &ifr) < 0)
-        die("%s ioctl", i->ifa_name);
+    assert(ioctl(s, SIOCGIFFLAGS, &ifr) >= 0, "%s ioctl", i->ifa_name);
 
     const bool link = (ifr.ifr_flags & IFF_UP) && (ifr.ifr_flags & IFF_RUNNING);
     close(s);

lib/udp.c
@@ -21,7 +21,7 @@ static inline void udp_log(const struct udp_hdr * const udp)
 #endif
 
 // Receive a UDP packet.
-void udp_rx(struct warpcore * const w, char * const buf, const uint32_t src)
+void udp_rx(struct warpcore * const w, void * const buf, const uint32_t src)
 {
     const struct ip_hdr * const ip = eth_data(buf);
     struct udp_hdr * const udp = ip_data(buf);
@@ -50,8 +50,7 @@ void udp_rx(struct warpcore * const w, char * const buf, const uint32_t src)
 
     // grab an unused iov for the data in this packet
     struct w_iov * const i = STAILQ_FIRST(&w->iov);
-    if (unlikely(i == 0))
-        die("out of spare bufs");
+    assert(i != 0, "out of spare bufs");
     struct netmap_ring * const rxr = NETMAP_RXRING(w->nif, w->cur_rxr);
     struct netmap_slot * const rxs = &rxr->slot[rxr->cur];
     STAILQ_REMOVE_HEAD(&w->iov, next);
@@ -94,7 +93,7 @@ void udp_tx(struct w_sock * const s)
         struct w_iov * const v = STAILQ_FIRST(&s->ov);
 
         // copy template header into buffer and fill in remaining fields
-        char * const buf = IDX2BUF(s->w, v->idx);
+        void * const buf = IDX2BUF(s->w, v->idx);
         memcpy(buf, s->hdr, s->hdr_len);
 
         struct udp_hdr * const udp = ip_data(buf);

lib/udp.h
@@ -15,6 +15,6 @@ struct w_sock;
 struct w_iov;
 
 extern void
-udp_rx(struct warpcore * const w, char * const buf, const uint32_t src);
+udp_rx(struct warpcore * const w, void * const buf, const uint32_t src);
 
 extern void udp_tx(struct w_sock * const s);

commit 557568ec55ab3813f1e92e748c38dbf4b95ae277

Fix bug introduced in 4a5df912b2de69f46d1cbfbebe1c996e493169f0

lib/warpcore.c
@@ -438,7 +438,8 @@ struct warpcore * w_init(const char * const ifname)
             }
         }
         freeifaddrs(ifap);
-        sleep(1);
+        // sleep for a bit, so we don't burn the CPU when link is down
+        usleep(50);
     }
     assert(w->ip != 0 && w->mask != 0 && w->mtu != 0 && !IS_ZERO(w->mac),
            "%s: cannot obtain needed interface information", ifname);
@@ -519,7 +520,7 @@ struct warpcore * w_init(const char * const ifname)
     SLIST_INIT(&w->sock);
 
     // allocate socket pointers
-    assert((w->udp = calloc(UINT16_MAX, sizeof(struct w_sock *))) == 0,
+    assert((w->udp = calloc(UINT16_MAX, sizeof(struct w_sock *))) != 0,
            "cannot allocate UDP sockets");
 
     // do the common system setup which is also useful for non-warpcore