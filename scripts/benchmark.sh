cbe5dd6a1cf1579680b79db6f5e6e8c173557147
Lars Eggert <lars@eggert.org>
2016-11-08 16:27:49 +0100

    Fix for vagrant

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -2,28 +2,32 @@
 
 set -e
 
-key=/vagrant/scripts/vagrant
 loops=1000
 # busywait=-b
-# aim=0
 
-peer=10.0.2.15
-iface=enp0s8
-piface=enp0s8
-peerip=$(ssh -i $key ubuntu@$peer "/sbin/ifconfig $piface" | sed -e s/addr:// -e s/^[[:space:]]*//g | grep 'inet ' | cut -f 2 -d' ')
+peer=192.168.101.2
+iface=enp0s9
+piface=enp0s9
+
+ssh="ssh $peer -q"
+
+peerip=$($ssh "/sbin/ifconfig $piface" | \
+         sed -e s/addr:// -e s/^[[:space:]]*//g | \
+         grep 'inet ' | cut -f 2 -d' ')
 
 run () {
         local flag
         if [ "$1" == "kern" ]; then
                 flag=-k
         fi
-        local cmd="/vagrant/examples/warpping -i $iface -d $peerip -l $loops $busywait"
+        local cmd="/vagrant/examples/warpping -i $iface -d $peerip \
+                   -l $loops $busywait"
         # for (( size=16; size <= 1458; size+=103)); do
         for (( size=16; size <= 1458; size+=303)); do
                 echo "Running $1 size $size"
                 $cmd $flag -s $size >> "$1.txt" 2> "warpping.$1.log"
         done
-        echo "nsec      size" > "$1.new"
+        printf "nsec\tsize\n" > "$1.new"
         grep -v nsec "$1.txt" >> "$1.new"
         mv "$1.new" "$1.txt"
 }
@@ -41,11 +45,11 @@ fi
 
 rm kern*.txt warp*.txt ./*.log > /dev/null 2>&1 || true
 
-ssh -i $key ubuntu@$peer "pkill -f warpinetd; ps -ef | grep warp" || true
-
-ssh -i $key ubuntu@$peer "cd /vagrant; /vagrant/examples/warpinetd -i $piface $busywait > warpinetd.log &" || true
+$ssh "pkill -f warpinetd" || true
+$ssh "nohup /vagrant/examples/warpinetd -i $piface $busywait \
+        > /vagrant/warpinetd.log 2>&1 &" || true
 run warp
-ssh -i $key ubuntu@$peer "pkill -f warpinetd; ps -ef | grep warp" || true
+$ssh "pkill -f warpinetd" || true
 
-sleep 5
+sleep 3
 run kern
