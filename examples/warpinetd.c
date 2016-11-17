4ee1482186dae5341f8ee1f6fda9cc7ba2775eab
Lars Eggert <lars@eggert.org>
2016-11-17 17:20:34 +0900

    Simplify warpinetd

--- examples/warpinetd.c
+++ examples/warpinetd.c
@@ -46,78 +46,73 @@ int main(int argc, char * argv[])
     struct warpcore * w = w_init(ifname, 0);
 
     // start the inetd-like "small services"
-    struct w_sock * ech = w_bind(w, IP_P_UDP, htons(7));
-    struct w_sock * dsc = w_bind(w, IP_P_UDP, htons(9));
-    struct w_sock * dtm = w_bind(w, IP_P_UDP, htons(13));
-    struct w_sock * tme = w_bind(w, IP_P_UDP, htons(37));
-    struct pollfd fds[] = {{.fd = w_fd(ech), .events = POLLIN},
-                           {.fd = w_fd(dsc), .events = POLLIN},
-                           {.fd = w_fd(dtm), .events = POLLIN},
-                           {.fd = w_fd(tme), .events = POLLIN}};
+    struct w_sock * const srv[] = {
+        w_bind(w, IP_P_UDP, htons(7)), w_bind(w, IP_P_UDP, htons(9)),
+        w_bind(w, IP_P_UDP, htons(13)), w_bind(w, IP_P_UDP, htons(37))};
+    const uint16_t n = sizeof(srv) / sizeof(struct w_sock *);
+    struct pollfd fds[] = {{.fd = w_fd(srv[0]), .events = POLLIN},
+                           {.fd = w_fd(srv[1]), .events = POLLIN},
+                           {.fd = w_fd(srv[2]), .events = POLLIN},
+                           {.fd = w_fd(srv[3]), .events = POLLIN}};
 
     while (1) {
         w_kick_rx(w);
         if (busywait == false)
-            poll(fds, 4, 1000);
+            poll(fds, n, 1000);
 
-        // echo service
-        struct w_iov * i = w_rx(ech);
-        while (i) {
-            warn(info, "echo %d bytes in buf %d", i->len, i->idx);
-            // echo the data
-            struct w_iov * o = w_tx_alloc(ech, i->len);
-            memcpy(o->buf, i->buf, i->len);
-            w_connect(ech, i->src, i->sport);
-            w_tx(ech);
-            i = STAILQ_NEXT(i, next);
+        for (uint16_t s = 0; s < n; s++) {
+            struct w_iov * i = w_rx(srv[s]);
+            uint32_t len = 0;
+
+            while (i) {
+                switch (s) {
+                case 0: { // echo
+                    struct w_iov * o = w_tx_alloc(srv[s], i->len);
+                    memcpy(o->buf, i->buf, i->len);
+                    w_connect(srv[s], i->src, i->sport);
+                    w_tx(srv[s]);
+                    break;
+                }
+
+                case 1: // discard
+                    break;
+
+                case 2: { // daytime
+                    const time_t t = time(0);
+                    const char * c = ctime(&t);
+                    const uint32_t l = (uint32_t)strlen(c);
+                    struct w_iov * o = w_tx_alloc(srv[s], l);
+                    memcpy(o->buf, c, l);
+                    w_connect(srv[s], i->src, i->sport);
+                    w_tx(srv[s]);
+                    break;
+                }
+
+                case 3: { // time
+                    const time_t t = time(0);
+                    struct w_iov * o = w_tx_alloc(srv[s], sizeof(time_t));
+                    *(time_t *)o->buf = htonl(t);
+                    w_connect(srv[s], i->src, i->sport);
+                    w_tx(srv[s]);
+                    break;
+                }
+
+                default:
+                    die("unknown service");
+                }
+
+                len += i->len;
+                i = STAILQ_NEXT(i, next);
+            }
+
+            w_rx_done(srv[s]);
+            if (len)
+                warn(info, "echo %d byte%c", len, plural(len));
         }
-        w_rx_done(ech);
+    }
 
-        // discard service
-        i = w_rx(dsc);
-        while (i) {
-            warn(info, "discard %d bytes in buf %d", i->len, i->idx);
-            // discard the data
-            i = STAILQ_NEXT(i, next);
-        }
-        w_rx_done(dsc);
-
-        // daytime service
-        i = w_rx(dtm);
-        while (i) {
-            warn(info, "daytime %d bytes in buf %d", i->len, i->idx);
-            const time_t t = time(0);
-            const char * c = ctime(&t);
-            const uint32_t l = (uint32_t)strlen(c);
-            struct w_iov * o = w_tx_alloc(dtm, l);
-            memcpy(o->buf, c, l);
-            w_connect(dtm, i->src, i->sport);
-            w_tx(dtm);
-            i = STAILQ_NEXT(i, next);
-        }
-        w_rx_done(dtm);
-
-        // time service
-        i = w_rx(tme);
-        while (i) {
-            warn(info, "time %d bytes in buf %d", i->len, i->idx);
-            const time_t t = time(0);
-            struct w_iov * o = w_tx_alloc(tme, sizeof(uint32_t));
-            uint32_t * const b = (uint32_t *)o->buf;
-            *b = htonl((uint32_t)t);
-            w_connect(tme, i->src, i->sport);
-            w_tx(tme);
-            i = STAILQ_NEXT(i, next);
-        }
-        w_rx_done(tme);
-    };
-
-    // w_close(ech);
-    // w_close(dsc);
-    // w_close(dtm);
-    // w_close(tme);
-
+    // for (uint16_t s = 0; s < n; s++)
+    //     w_close(srv[s]);
     // w_cleanup(w);
-
     // return 0;
 }
