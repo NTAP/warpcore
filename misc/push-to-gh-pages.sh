c65fb2c3ccb90eadf2ced99e13cf35d4771e30bf
Lars Eggert <lars@eggert.org>
2017-01-17 10:43:19 +0100

    Fix

--- misc/push-to-gh-pages.sh
+++ misc/push-to-gh-pages.sh
@@ -47,5 +47,5 @@ if [ -d "html" ] && [ -f "html/index.html" ]; then
     # Force push to the remote gh-pages branch. The ouput is redirected to
     # /dev/null to hide any sensitive credential data that might otherwise be
     # exposed.
-    git push --force "https://${GH_REPO_TOKEN}@${GH_REPO_REF}" > /dev/null 2>&1
+    echo git push --force "https://${GH_REPO_TOKEN}@${GH_REPO_REF}" > /dev/null 2>&1
 fi
