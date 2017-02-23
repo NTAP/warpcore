40448804216975f0969fe502dd129a088f9cccd3
Lars Eggert <lars@eggert.org>
2017-02-23 15:12:13 +0100

    Add the beginnings of a unit-test/microbenchmark suite based on https://github.com/google/benchmark

---
+++ test/in_cksum.cc
@@ -0,0 +1,50 @@
+// Copyright (c) 2014-2017, NetApp, Inc.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+//    this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#include <benchmark/benchmark_api.h>
+#include <cstdint>
+#include <cstring>
+
+extern "C" {
+#include "ip.h"
+}
+
+
+static void BM_in_cksum(benchmark::State & state)
+{
+    const uint16_t len = uint16_t(state.range(0));
+    auto * buf = new char[len];
+    memset(buf, 'x', len);
+    while (state.KeepRunning())
+        in_cksum(buf, len);
+    state.SetBytesProcessed(state.iterations() * len);
+    delete[] buf;
+}
+
+
+BENCHMARK(BM_in_cksum)->RangeMultiplier(2)->Range(4, 2048);
+
+
+BENCHMARK_MAIN()
