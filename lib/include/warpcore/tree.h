33685613343d2e09562a82ce9c942903db26f230
Lars Eggert <lars@eggert.org>
2018-02-01 12:36:50 +0100

    Try and suppress "unused function" warnings through a dummy function pointer.

--- lib/include/warpcore/tree.h
+++ lib/include/warpcore/tree.h
@@ -132,6 +132,7 @@
     static __inline struct type * name##_splay_find(struct name * head,        \
                                                     struct type * elm)         \
     {                                                                          \
+        (void)name##_splay_find;                                               \
         if (splay_empty(head))                                                 \
             return (NULL);                                                     \
         name##_splay(head, elm);                                               \
@@ -143,6 +144,7 @@
     static __inline struct type * name##_splay_next(struct name * head,        \
                                                     struct type * elm)         \
     {                                                                          \
+        (void)name##_splay_next;                                               \
         name##_splay(head, elm);                                               \
         if (splay_right(elm, field) != NULL) {                                 \
             elm = splay_right(elm, field);                                     \
@@ -157,6 +159,7 @@
     static __inline struct type * name##_splay_prev(struct name * head,        \
                                                     struct type * elm)         \
     {                                                                          \
+        (void)name##_splay_prev;                                               \
         name##_splay(head, elm);                                               \
         if (splay_left(elm, field) != NULL) {                                  \
             elm = splay_left(elm, field);                                      \
@@ -171,6 +174,7 @@
     static __inline struct type * name##_splay_min_max(struct name * head,     \
                                                        int val)                \
     {                                                                          \
+        (void)name##_splay_min_max;                                            \
         name##_splay_minmax(head, val);                                        \
         return (splay_root(head));                                             \
     }
