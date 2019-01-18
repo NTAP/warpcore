NTAP/warpcore

commit 19aaea246ca23fe3bd3f314d6de06169ddb36ce3

Fix the fuzzer

lib/src/udp.c
@@ -33,7 +33,6 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <string.h>
-#include <sys/param.h>
 
 #include "arp.h"
 #include "backend.h"
@@ -84,16 +83,21 @@ void
 {
     const struct ip_hdr * const ip = (const void *)eth_data(buf);
     struct udp_hdr * const udp = (void *)ip_data(buf);
-    const uint16_t udp_len =
-        MIN(ntohs(udp->len),
-            s->len - sizeof(struct eth_hdr) - sizeof(struct ip_hdr));
-    udp_log(udp);
 
-    if (unlikely(udp_len < 8)) {
-        warn(WRN, "received invalid UDP len %u", udp_len);
+    if (unlikely(s->len <=
+                 sizeof(struct eth_hdr) + sizeof(*ip) + sizeof(*udp))) {
+#ifndef FUZZING
+        warn(WRN, "IP len %u too short for UDP", s->len);
+#endif
         return;
     }
 
+    const uint16_t udp_len =
+        s->len - sizeof(struct eth_hdr) - sizeof(*ip) - sizeof(*udp);
+    udp_log(udp);
+
+
+#ifndef FUZZING
     if (likely(udp->cksum)) {
         // validate the checksum
         const uint16_t cksum = udp_cksum(ip, udp_len + sizeof(*ip));
@@ -103,6 +107,7 @@ void
             return;
         }
     }
+#endif
 
     struct w_sock * ws = w_get_sock(w, udp->dport);
     if (unlikely(ws == 0)) {

lib/src/arp.c
@@ -39,7 +39,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
+
+#ifndef FUZZING
 #include <unistd.h>
+#endif
 
 #include "arp.h"
 #include "backend.h"
@@ -147,7 +150,9 @@ arp_is_at(struct w_engine * const w, uint8_t * const buf)
     const uint32_t orig_idx = v->idx;
     eth_tx(w, v, sizeof(*reply));
     do {
+#ifndef FUZZING
         usleep(100);
+#endif
         w_nic_tx(w);
     } while (v->idx != orig_idx);
     sq_insert_head(&w->iov, v, next);
@@ -217,7 +222,9 @@ struct ether_addr
         const uint32_t orig_idx = v->idx;
         eth_tx(w, v, sizeof(*eth) + sizeof(*arp));
         do {
+#ifndef FUZZING
             usleep(100);
+#endif
             w_nic_tx(w);
         } while (v->idx != orig_idx);
         sq_insert_head(&w->iov, v, next);

lib/src/icmp.c
@@ -33,7 +33,10 @@
 #include <stdint.h>
 #include <string.h>
 #include <sys/param.h>
+
+#ifndef FUZZING
 #include <unistd.h>
+#endif
 
 #include "backend.h"
 #include "eth.h"
@@ -128,7 +131,9 @@ void icmp_tx(struct w_engine * const w,
     const uint32_t orig_idx = v->idx;
     ip_tx(w, v, sizeof(*dst_icmp) + data_len);
     do {
+#ifndef FUZZING
         usleep(100);
+#endif
         w_nic_tx(w);
     } while (v->idx != orig_idx);
     sq_insert_head(&w->iov, v, next);
@@ -148,12 +153,19 @@ void icmp_tx(struct w_engine * const w,
 /// @param      buf   Incoming packet.
 ///
 void icmp_rx(struct w_engine * const w,
-             struct netmap_slot * const s,
+             struct netmap_slot * const s
+#ifdef FUZZING
+             __attribute__((unused))
+#endif
+             ,
              uint8_t * const buf)
 {
     struct icmp_hdr * const icmp = (void *)ip_data(buf);
+#ifndef FUZZING
     rwarn(DBG, 10, "received ICMP type %d, code %d", icmp->type, icmp->code);
+#endif
 
+#ifndef FUZZING
     // validate the ICMP checksum
     struct ip_hdr * const ip = (void *)eth_data(buf);
     const uint16_t icmp_len =
@@ -164,6 +176,7 @@ void icmp_rx(struct w_engine * const w,
         warn(WRN, "invalid ICMP checksum, received 0x%04x", ntohs(icmp->cksum));
         return;
     }
+#endif
 
     switch (icmp->type) {
     case ICMP_TYPE_ECHO:
@@ -193,11 +206,17 @@ void icmp_rx(struct w_engine * const w,
             break;
         }
         default:
-            die("unhandled ICMP code %d", icmp->code);
+#ifndef FUZZING
+            rwarn(WRN, 10, "unhandled ICMP code %d", icmp->code)
+#endif
+                ;
         }
         break;
     }
     default:
-        die("unhandled ICMP type %d", icmp->type);
+#ifndef FUZZING
+        rwarn(WRN, 10, "unhandled ICMP type %d", icmp->type)
+#endif
+            ;
     }
 }

lib/src/ip.c
@@ -103,14 +103,14 @@ void
         return;
     }
 
+#ifndef FUZZING
     // validate the IP checksum
     if (unlikely(ip_cksum(ip, sizeof(*ip)) != 0)) {
-#ifndef FUZZING
         warn(WRN, "invalid IP checksum, received 0x%04x != 0x%04x",
              ntohs(ip->cksum), ip_cksum(ip, sizeof(*ip)));
-#endif
         return;
     }
+#endif
 
     if (unlikely(ip_hl(ip) != 20)) {
         // TODO: handle IP options

lib/src/warpcore.c
@@ -375,7 +375,13 @@ w_init(const char * const ifname, const uint32_t rip, const uint32_t nbufs)
     // init state for w_rand()
     struct timeval now;
     gettimeofday(&now, 0);
-    kr_srand_r(&w_rand_state, (uint64_t)now.tv_usec);
+    kr_srand_r(&w_rand_state,
+#ifndef FUZZING
+               (uint64_t)now.tv_usec
+#else
+               0
+#endif
+    );
 
     // construct interface name of a netmap pipe for this interface
     char pipe[IFNAMSIZ];

test/fuzz.c
@@ -31,6 +31,9 @@
 #include <sys/param.h>
 #include <sys/types.h>
 
+#define klib_unused
+
+#include <khash.h>
 #include <net/netmap.h>
 #include <net/netmap_user.h>
 #include <warpcore/warpcore.h>
@@ -53,6 +56,8 @@ int LLVMFuzzerInitialize(int * argc __attribute__((unused)),
 #ifndef NDEBUG
     util_dlevel = DBG;
 #endif
+
+    w.sock = kh_init(sock);
 
     // init the interface shim
     i = calloc(1, sizeof(*i) + 8192);
@@ -95,11 +100,11 @@ int LLVMFuzzerTestOneInput(const uint8_t * data, const size_t size)
     r->cur = r->head = 1;
     r->tail = 0;
     struct netmap_slot * const s = &r->slot[r->cur];
-    char * const buf = NETMAP_BUF(r, r->cur);
+    uint8_t * const buf = (uint8_t *)NETMAP_BUF(r, r->cur);
 
     s->len = (uint16_t)MIN(size, r->nr_buf_size);
     memcpy(buf, data, s->len);
 
-    eth_rx(&w, r);
+    eth_rx(&w, s, buf);
     return 0;
 }

commit 091eadf8d96bec0fad8328ea4d1edc1987489366

Fix UDP length calculation bug introduced in 19aaea246ca23fe3bd3f314d6de06169ddb36ce3

lib/src/udp.c
@@ -92,8 +92,7 @@ void
         return;
     }
 
-    const uint16_t udp_len =
-        s->len - sizeof(struct eth_hdr) - sizeof(*ip) - sizeof(*udp);
+    const uint16_t udp_len = s->len - sizeof(struct eth_hdr) - sizeof(*ip);
     udp_log(udp);
 
 