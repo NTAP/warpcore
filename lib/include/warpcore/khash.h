42278427a1c7bae9f755ccdc9e6b5e0de416f915
Lars Eggert <lars@eggert.org>
2019-08-14 09:16:25 +0300

    Re-add stock khash.h

--- lib/include/warpcore/khash.h
+++ lib/include/warpcore/khash.h
@@ -189,7 +189,7 @@ typedef khint_t khiter_t;
 #define kfree(P) free(P)
 #endif
 
-#define __HASH_UPPER(x) ((((x) * 3) >> 2) + 1)
+static const double __ac_HASH_UPPER = 0.77;
 
 #define __KHASH_TYPE(name, khkey_t, khval_t) \
 	typedef struct kh_##name##_s { \
@@ -248,7 +248,7 @@ typedef khint_t khiter_t;
 		{																\
 			kroundup32(new_n_buckets); 									\
 			if (new_n_buckets < 4) new_n_buckets = 4;					\
-			if (h->size >= __HASH_UPPER(new_n_buckets)) j = 0;	/* requested size is too small */ \
+			if (h->size >= (khint_t)(new_n_buckets * __ac_HASH_UPPER + 0.5)) j = 0;	/* requested size is too small */ \
 			else { /* hash table size to be changed (shrink or expand); rehash */ \
 				new_flags = (khint32_t*)kmalloc(__ac_fsize(new_n_buckets) * sizeof(khint32_t));	\
 				if (!new_flags) return -1;								\
@@ -300,7 +300,7 @@ typedef khint_t khiter_t;
 			h->flags = new_flags;										\
 			h->n_buckets = new_n_buckets;								\
 			h->n_occupied = h->size;									\
-			h->upper_bound = __HASH_UPPER(h->n_buckets); \
+			h->upper_bound = (khint_t)(h->n_buckets * __ac_HASH_UPPER + 0.5); \
 		}																\
 		return 0;														\
 	}																	\
