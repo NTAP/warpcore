bed6fc7fd1a7cc316821165c6739fec40418a7c8
Lars Eggert <lars@eggert.org>
2020-07-29 11:48:45 +0300

    Fix iwyu warning

--- lib/src/backend_sock.c
+++ lib/src/backend_sock.c
@@ -40,10 +40,13 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
-#include <string.h>
 #include <sys/param.h>
 #include <sys/socket.h>
 #include <unistd.h>
+
+#ifndef NDEBUG
+#include <string.h>
+#endif
 
 #include <warpcore/warpcore.h>
 
